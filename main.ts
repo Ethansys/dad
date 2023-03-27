32fef4df13cd3e0692551d820a6edec84b59cb9c
Ethansys <129080364+Ethansys@users.noreply.github.com>
2023-03-27 22:14:05 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+basic.showString("Hello! Are you excited?")
+basic.showString("If you say yes, then we will start now!")
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    . . . . .
+    `)
+music.playMelody("G B A G C5 B A B ", 120)
+basic.showString("Dad, love you.")
+music.playMelody("E B C5 A B G A F ", 120)
+basic.showIcon(IconNames.Heart)
+music.playMelody("A F E F D G E F ", 120)
+basic.showString("Thank you, dad!")
+music.playMelody("E D G F B A C5 B ", 120)
+basic.showIcon(IconNames.Heart)
+music.playMelody("C5 B A G F E D C ", 120)
+basic.showString("Thank you for watching this! hope you have a wonderful day!")
+basic.showString("By Ethan system 1,2,3,4,5,6,7,8,9,10")
+basic.showIcon(IconNames.Heart)
+basic.showString("Thank you!")
+music.playMelody("E D G F B A C5 B ", 120)
+basic.showString("-Thank you have a wonderful day!:)-")
+basic.forever(function () {
+	
+})
